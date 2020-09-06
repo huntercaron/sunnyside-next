3bb815759204005fa73851f2675ce856af8bded0
Hunter Caron <huntercaron@me.com>
2020-09-06 21:59:14 +0200

    sanity setup

---
+++ cms/schemas/project.js
@@ -0,0 +1,10 @@
+export default {
+    name: "project",
+    type: "document",
+    title: "Project",
+    fields: [
+        { name: "title", type: "string", title: "Title" },
+        { name: "description", type: "text", title: "Description" },
+        { name: "gallery", type: "array", of: [{ type: "image" }] },
+    ],
+}
