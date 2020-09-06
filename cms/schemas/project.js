629bee52685df886b40d9a91c9d4570f204c3483
Hunter Caron <huntercaron@me.com>
2020-09-06 22:21:16 +0200

    sluggin

--- cms/schemas/project.js
+++ cms/schemas/project.js
@@ -4,7 +4,26 @@ export default {
     title: "Project",
     fields: [
         { name: "title", type: "string", title: "Title" },
+        {
+            name: "slug",
+            type: "slug",
+            title: "Slug",
+            options: {
+                source: "title",
+                maxLength: 200, // will be ignored if slugify is set
+                slugify: (input) =>
+                    input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
+            },
+        },
         { name: "description", type: "text", title: "Description" },
-        { name: "gallery", type: "array", of: [{ type: "image" }] },
+        {
+            name: "gallery",
+            type: "array",
+
+            options: {
+                layout: "grid",
+            },
+            of: [{ type: "image", title: "Image", hotspot: true }],
+        },
     ],
 }
