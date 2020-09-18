44d4235603b7b20d53c3ff0f78d9f18de3025d6b
Hunter Caron <huntercaron@me.com>
2020-09-18 15:09:42 +0200

    updating cms path

--- cms/src/resolveProductionUrl.ts
+++ cms/src/resolveProductionUrl.ts
@@ -1,5 +1,5 @@
 const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET
-const projectUrl = "https://sunnyside.vercel.app"
+const projectUrl = "https://sunnyside.cool"
 
 export default function resolveProductionUrl(document) {
     return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`
